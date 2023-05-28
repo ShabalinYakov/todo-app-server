7c7dc344d55789d61173e1d6d5d74e08247489c3
Shabalin_Yakov <shab.yakov@ya.ru>
2023-05-28 17:22:17 +0500

    feat: added type TokenPayload and type TokenData

--- src/interfaces/auth.interfaces.ts
+++ src/interfaces/auth.interfaces.ts
@@ -2,3 +2,14 @@ export interface LoginPayload {
   login: string;
   password: string;
 }
+
+export interface TokenPayload {
+  id: string;
+  login: string;
+  is_leader: boolean;
+}
+
+export interface TokenData {
+  refreshToken: string;
+  accessToken: string;
+}
