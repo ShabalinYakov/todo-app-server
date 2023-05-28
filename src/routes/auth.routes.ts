026cde484a3a1a2e0e9c55d217a9839936b520a6
Shabalin_Yakov <shab.yakov@ya.ru>
2023-05-28 17:01:06 +0500

    refactor: added method login for path login

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
+import { authController } from '../controllers/auth.controller';
 
 const authRouter = Router({ mergeParams: true });
 
-authRouter.post('/login', () => 'authController');
+authRouter.post('/login', authController.login);
 
 export default authRouter;
