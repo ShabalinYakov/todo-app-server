deabe55c7a9854612a0e6888191491287a308303
Shabalin_Yakov <shab.yakov@ya.ru>
2023-05-28 23:56:37 +0500

    refactor: style and status code for auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,29 +1,30 @@
 import { Request, Response, NextFunction } from 'express';
-import ApiError from '../exceptions/api-error';
-import { tokenService } from '../services/token.service';
+
 import { RequestWithUser } from '../interfaces/auth.interfaces';
+import { tokenService } from '../services/token.service';
+import ApiError from '../exceptions/api-error';
 
 const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   try {
     const authorizationHeader = req.headers.authorization;
     if (!authorizationHeader) {
-      return next(new ApiError(500, 'Войдите'));
+      return next(ApiError.UnauthorizedError());
     }
 
     const accessToken = authorizationHeader.split(' ')[1];
     if (!accessToken) {
-      return next(new ApiError(500, 'Войдите'));
+      return next(ApiError.UnauthorizedError());
     }
 
     const userData = tokenService.validateAccessToken(accessToken);
     if (!userData) {
-      return next(new ApiError(500, 'Войдите'));
+      return next(ApiError.UnauthorizedError());
     }
 
     (req as RequestWithUser).user = userData;
     next();
   } catch (error) {
-    return next(new ApiError(500, 'Войдите'));
+    return next(ApiError.UnauthorizedError());
   }
 };
 export default authMiddleware;
