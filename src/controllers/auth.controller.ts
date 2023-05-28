0fa3f85979bb2fc600e0d2bf89638e3dce53c5e7
Shabalin_Yakov <shab.yakov@ya.ru>
2023-05-28 22:54:30 +0500

    feat: added validate login and password

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
 import { authService } from '../services/auth.service';
+import { validationResult } from 'express-validator';
 
 class AuthController {
   async login(req: Request, res: Response, next: NextFunction) {
     try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).send({
+          errors: { message: 'INVALID_DATA', code: 400 },
+        });
+      }
+
       const { login, password } = req.body;
       const response = await authService.login({ login, password });
 
