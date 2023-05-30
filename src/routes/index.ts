4902979494bb4d2caac1d523045f4656cf9f3a38
Shabalin_Yakov <shab.yakov@ya.ru>
2023-05-30 22:43:56 +0500

    feat: added test private route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import authRouter from './auth.routes';
+import testRoute from './test.routes';
 
 const router = Router({ mergeParams: true });
 
 router.use('/auth', authRouter);
+router.use('/test', testRoute);
 
 export default router;
