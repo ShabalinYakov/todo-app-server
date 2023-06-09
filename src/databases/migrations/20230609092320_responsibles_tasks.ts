a6985398768d9ce9ed1713657e25da7a4faeebff
Shabalin_Yakov <shab.yakov@ya.ru>
2023-06-09 15:08:45 +0500

    feat: added migration and seed for responsibles_tasks

---
+++ src/databases/migrations/20230609092320_responsibles_tasks.ts
@@ -0,0 +1,14 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return await knex.raw(`
+  CREATE TABLE responsibles_tasks (
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    task_id UUID REFERENCES tasks(id) ON DELETE CASCADE
+  );
+  `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return await knex.raw(`DROP TABLE responsibles_tasks`);
+}
