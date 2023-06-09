635916c10db362d46b2d5e6c6a9f4b7c72ba0816
Shabalin_Yakov <shab.yakov@ya.ru>
2023-06-09 15:03:39 +0500

    feat: added migration and seed for statuses

---
+++ src/databases/migrations/20230609082746_statuses.ts
@@ -0,0 +1,14 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return await knex.raw(`
+  CREATE TABLE statuses (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    name VARCHAR NOT NULL
+  );
+  `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return await knex.raw(`DROP TABLE statuses;`);
+}
