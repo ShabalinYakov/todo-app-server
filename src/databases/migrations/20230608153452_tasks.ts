62600749a9551e31c0a7ccf0af39e7a2b85e4d19
Shabalin_Yakov <shab.yakov@ya.ru>
2023-06-09 13:12:39 +0500

    feat: added migration for create tasks table

---
+++ src/databases/migrations/20230608153452_tasks.ts
@@ -0,0 +1,23 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return await knex.raw(`
+    CREATE TABLE tasks (
+      id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+      title VARCHAR NOT NULL,
+      description TEXT,
+      deadline VARCHAR NOT NULL,
+      created_at DATE DEFAULT CURRENT_DATE,
+      updated_at DATE DEFAULT CURRENT_DATE
+    );
+    
+    CREATE TRIGGER tasks_updated_at
+    BEFORE UPDATE ON tasks
+    FOR EACH ROW
+    EXECUTE PROCEDURE on_update_date();
+  `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return await knex.raw(`DROP TABLE tasks;`);
+}
