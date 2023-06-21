5c9a1978c8415e11f5ede0c96da200cf168f764f
Shabalin_Yakov <shab.yakov@ya.ru>
2023-06-21 13:51:26 +0500

    changeset: added endpoint tasks, priorities, statuses, leader

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
+
+import prioritiesRouter from './priorities.routes';
+import statusesRouter from './statuses.routes';
+import leaderRouter from './leader.routes';
+import tasksRouter from './tasks.routes';
 import authRouter from './auth.routes';
-import testRoute from './test.routes';
 
 const router = Router({ mergeParams: true });
 
 router.use('/auth', authRouter);
-router.use('/test', testRoute);
+router.use('/tasks', tasksRouter);
+router.use('/priorities', prioritiesRouter);
+router.use('/statuses', statusesRouter);
+router.use('/leader', leaderRouter);
 
 export default router;
